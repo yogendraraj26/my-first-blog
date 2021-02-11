1d09584b28a168a1a8e6a85ab9863c1a2d13c121
Yogi <yogendraand26@gmail.com>
2021-02-11 21:23:13 +0530

    Modified the blog with custom css.

--- myblog/views.py
+++ myblog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.all()
 
     return render(request,'myblog/post_list.html',{'posts':posts})
-    
+
